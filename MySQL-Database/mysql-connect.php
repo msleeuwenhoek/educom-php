73027c4170a9657e916f2707e14c7719c0260862
msleeuwenhoek <97388771+msleeuwenhoek@users.noreply.github.com>
2022-11-08 16:47:28 +0100

    mysql create table (#5)

--- MySQL-Database/mysql-connect.php
+++ MySQL-Database/mysql-connect.php
@@ -11,20 +11,29 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
+$dbname = "myDB";
+
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password);
+$conn = mysqli_connect($servername, $username, $password, $dbname);
 // Check connection
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
-} else {echo "Connected succesfully";}
+}
 
-// Create database
-$sql = "CREATE DATABASE myDB";
+// sql to create table
+$sql = "CREATE TABLE MyGuests (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+firstname VARCHAR(30) NOT NULL,
+lastname VARCHAR(30) NOT NULL,
+email VARCHAR(50),
+reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)";
+
 if (mysqli_query($conn, $sql)) {
-  echo "Database created successfully";
+  echo "Table MyGuests created successfully";
 } else {
-  echo "Error creating database: " . mysqli_error($conn);
+  echo "Error creating table: " . mysqli_error($conn);
 }
 
 mysqli_close($conn);
