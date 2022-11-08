95294341f1f05ee93011b5359266b38fee264d84
msleeuwenhoek <97388771+msleeuwenhoek@users.noreply.github.com>
2022-11-08 10:23:11 +0100

    syntax scope php (#1)

--- syntax.php
+++ syntax.php
@@ -7,25 +7,59 @@
 <body>
     <h1>My first PHP page</h1>
 
-    <?php 
-    $txt = "text";
-    echo "This is a $txt";
-    ?>
-    <br/>
+     <?php
+$x = 5; // global scope
 
-    <?php
-    echo "This is a " . $txt;
-    
-    ?>
-    <br/>
-    <?php
-    $x = 5;
-    $y = 7;
+function myTest() {
+  // using x inside this function will generate an error
+  echo "<p>Variable x inside function is: $x</p>";
+}
+myTest();
 
-    echo $x + $y;
-  
-    
-?>
+echo "<p>Variable x outside function is: $x</p>";
+?> 
+
+
+
+ <?php
+function myTest2() {
+  $y = 5; // local scope
+  echo "<p>Variable y inside function is: $y</p>";
+}
+myTest2();
+
+// using x outside the function will generate an error
+echo "<p>Variable y outside function is: $y</p>";
+?> 
+
+ <?php
+$z = 5;
+$p = 10;
+
+function myTest3() {
+  global $z, $p;
+  $p = $z + $p;
+}
+
+myTest3();
+echo $p; // outputs 15
+?> 
+
+
+
+<br/>
+
+ <?php
+function myTest4() {
+  static $s = 0;
+  echo $s;
+  $s++;
+}
+
+myTest4();
+myTest4();
+myTest4();
+?> 
    
 </body>
 </html>
