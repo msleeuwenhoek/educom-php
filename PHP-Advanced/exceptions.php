304d2b8d5245d7fa79fda63b1f51b3dc01e3bdc6
msleeuwenhoek <97388771+msleeuwenhoek@users.noreply.github.com>
2022-11-08 15:11:06 +0100

    php exceptions (#3)

---
+++ PHP-Advanced/exceptions.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+function divide($dividend, $divisor) {
+  if($divisor == 0) {
+    throw new Exception("Division by zero");
+  }
+  return $dividend / $divisor;
+}
+
+try {
+  echo divide(5, 0);
+} catch(Exception $e) {
+  echo "Unable to divide. ";
+} finally {
+  echo "Process complete.";
+}
+
+
+
+echo "<br/>";
+
+
+
+function divide3($dividend, $divisor) {
+  if($divisor == 0) {
+    throw new Exception("Division by zero", 1);
+  }
+  return $dividend / $divisor;
+}
+
+try {
+  echo divide3(5, 0);
+} catch(Exception $ex) {
+  $code = $ex->getCode();
+  $message = $ex->getMessage();
+  $file = $ex->getFile();
+  $line = $ex->getLine();
+  echo "Exception thrown in $file on line $line: [Code $code]
+  $message";
+}
+?> 
+</body>
+</html>
