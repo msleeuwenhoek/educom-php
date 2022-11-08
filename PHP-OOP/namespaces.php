c8c8d2b1d2a91609d0c6f5c5d2179fd9ff1027cf
msleeuwenhoek <97388771+msleeuwenhoek@users.noreply.github.com>
2022-11-08 16:06:08 +0100

    php namespaces

---
+++ PHP-OOP/namespaces.php
@@ -0,0 +1,27 @@
+<?php
+namespace Html;
+class Table {
+  public $title = "";
+  public $numRows = 0;
+  public function message() {
+    echo "<p>Table '{$this->title}' has {$this->numRows} rows.</p>";
+  }
+}
+
+
+use Html\Table as T;
+$table = new T();
+$table->title = "My table";
+$table->numRows = 5;
+?>
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$table->message();
+?>
+
+</body>
+</html> 
