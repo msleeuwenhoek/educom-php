1746c30bf59be0db3056291f60798e4f88047c94
msleeuwenhoek <97388771+msleeuwenhoek@users.noreply.github.com>
2022-11-08 10:39:02 +0100

    php strings (#1)

---
+++ strings.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>PHP String</title>
+</head>
+<body>
+    <?php
+echo strlen("Hello world!"); // outputs 12
+echo "<br/>";
+echo str_word_count("Hello world!"); // outputs 2
+echo "<br/>";
+echo strrev("Hello world!"); // outputs !dlrow olleH
+echo "<br/>";
+echo strpos("Hello world!", "world"); // outputs 6
+echo "<br/>";
+
+echo str_replace("world", "Dolly", "Hello world!"); // outputs Hello Dolly!
+?>  
+</body>
+</html>
