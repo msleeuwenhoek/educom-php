f809b4e6bc1ec5e8510034200b616e51c54a4471
msleeuwenhoek <97388771+msleeuwenhoek@users.noreply.github.com>
2022-11-08 16:44:01 +0100

    mysql create DB (#5)

--- MySQL-Database/mysql-connect.php
+++ MySQL-Database/mysql-connect.php
@@ -13,13 +13,21 @@ $username = "root";
 $password = "";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
-
+$conn = mysqli_connect($servername, $username, $password);
 // Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+} else {echo "Connected succesfully";}
+
+// Create database
+$sql = "CREATE DATABASE myDB";
+if (mysqli_query($conn, $sql)) {
+  echo "Database created successfully";
+} else {
+  echo "Error creating database: " . mysqli_error($conn);
 }
-echo "Connected successfully";
+
+mysqli_close($conn);
 ?> 
 </body>
 </html>
