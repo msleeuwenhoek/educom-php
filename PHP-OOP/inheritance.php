3df7c18871e6c40d9dddede2865a3b7e840849db
msleeuwenhoek <97388771+msleeuwenhoek@users.noreply.github.com>
2022-11-08 15:36:40 +0100

    php class inheritance (#4)

---
+++ PHP-OOP/inheritance.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Class inheritance PHP</title>
+</head>
+<body>
+ <?php
+class Fruit {
+  public $name;
+  public $color;
+  public function __construct($name, $color) {
+    $this->name = $name;
+    $this->color = $color;
+  }
+  protected function intro() {
+    echo "The fruit is {$this->name} and the color is {$this->color}.";
+  }
+}
+
+class Strawberry extends Fruit {
+  public function message() {
+    echo "Am I a fruit or a berry? ";
+    // Call protected method from within derived class - OK
+    $this -> intro();
+  }
+}
+
+$strawberry = new Strawberry("Strawberry", "red"); // OK. __construct() is public
+$strawberry->message(); // OK. message() is public and it calls intro() (which is protected) from within the derived class
+
+echo "<br/>";
+echo "<br/>";
+echo "<br/>";
+
+
+class Fruit2 {
+  public $name;
+  public $color;
+  public function __construct($name, $color) {
+    $this->name = $name;
+    $this->color = $color;
+  }
+  public function intro() {
+    echo "The fruit is {$this->name} and the color is {$this->color}.";
+  }
+}
+
+class Strawberry2 extends Fruit2 {
+  public $weight;
+  public function __construct($name, $color, $weight) {
+    $this->name = $name;
+    $this->color = $color;
+    $this->weight = $weight;
+  }
+  public function intro() {
+    echo "The fruit is {$this->name}, the color is {$this->color}, and the weight is {$this->weight} gram.";
+  }
+}
+
+$strawberry = new Strawberry2("Strawberry", "red", 50);
+$strawberry->intro();
+
+ 
+final class Fruit3 {
+  // some code
+}
+
+// will result in error
+//class Strawberry3 extends Fruit3 {
+  // some code
+//}
+
+
+class Fruit4 {
+  final public function intro() {
+    // some code
+  }
+}
+
+class Strawberry4 extends Fruit4 {
+  // will result in error
+  //public function intro() {
+    // some code
+  }
+
+?> 
+    
+</body>
+</html>
