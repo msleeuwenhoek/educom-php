f0053aa8ee8111e6d65d0760e23a3c4ac97f558c
msleeuwenhoek <97388771+msleeuwenhoek@users.noreply.github.com>
2022-11-08 16:49:07 +0100

    mysql insert data (#5)

--- MySQL-Database/mysql-connect.php
+++ MySQL-Database/mysql-connect.php
@@ -21,19 +21,13 @@ if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
 
-// sql to create table
-$sql = "CREATE TABLE MyGuests (
-id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-firstname VARCHAR(30) NOT NULL,
-lastname VARCHAR(30) NOT NULL,
-email VARCHAR(50),
-reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-)";
+$sql = "INSERT INTO MyGuests (firstname, lastname, email)
+VALUES ('John', 'Doe', 'john@example.com')";
 
 if (mysqli_query($conn, $sql)) {
-  echo "Table MyGuests created successfully";
+  echo "New record created successfully";
 } else {
-  echo "Error creating table: " . mysqli_error($conn);
+  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 }
 
 mysqli_close($conn);
