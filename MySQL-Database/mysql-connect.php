cce78664abc470b1679ec2848dc30ae238f9d710
msleeuwenhoek <97388771+msleeuwenhoek@users.noreply.github.com>
2022-11-09 10:06:51 +0100

    mysql order data (#5)

--- MySQL-Database/mysql-connect.php
+++ MySQL-Database/mysql-connect.php
@@ -14,6 +14,7 @@ $password = "";
 $dbname = "myDB";
 
 
+
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
@@ -21,14 +22,16 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT id, firstname, lastname FROM MyGuests WHERE lastname='Doe'";
+$sql = "SELECT id, firstname, lastname FROM MyGuests ORDER BY lastname";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
+  echo "<table><tr><th>ID</th><th>Name</th></tr>";
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+    echo "<tr><td>".$row["id"]."</td><td>".$row["firstname"]." ".$row["lastname"]."</td></tr>";
   }
+  echo "</table>";
 } else {
   echo "0 results";
 }
