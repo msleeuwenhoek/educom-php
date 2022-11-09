4ca040f7006a0068577643adf3680447866f0f5f
msleeuwenhoek <97388771+msleeuwenhoek@users.noreply.github.com>
2022-11-09 10:14:15 +0100

    mysql limit data (#5)

--- MySQL-Database/mysql-connect.php
+++ MySQL-Database/mysql-connect.php
@@ -21,12 +21,16 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "UPDATE MyGuests SET lastname='Doe' WHERE id=4";
+$sql = "SELECT * FROM MyGuests LIMIT 4, 2";
+$result = $conn->query($sql);
 
-if ($conn->query($sql) === TRUE) {
-  echo "Record updated successfully";
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+  }
 } else {
-  echo "Error updating record: " . $conn->error;
+  echo "0 results";
 }
 
 $conn->close();
